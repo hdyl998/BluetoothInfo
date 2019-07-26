1c95c419e8e35abb5a14e48d602dff748ffda49c
liugd <986850427@qq.com>
2019-07-26 08:34:35 +0800

    111

--- 资料/BrainLink thinkgear系列开发资料/HelloEEGActivity.java
+++ 资料/BrainLink thinkgear系列开发资料/HelloEEGActivity.java
@@ -5,7 +5,6 @@ import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
